008cd0b73b86f06c2b768c451b026953ece11da6
Billi Ramirez <ramirezbilly42@outlook.com>
2018-10-27 23:47:11 -0600

    Uploading the React-router course

---
+++ src/entries/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { hydrate } from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import App from '../pages/containers/app';
+
+
+const homeContainer = document.getElementById('home-container')
+
+
+// render(
+hydrate(
+  <BrowserRouter
+
+  >
+   <App/>
+  </BrowserRouter>
+, homeContainer);
+
---
+++ src/pages/components/not-found.js
@@ -0,0 +1,48 @@
+import React, {PureComponent} from 'react';
+import './generic-page.css';
+
+import {withRouter} from 'react-router'
+class NotFound extends PureComponent{
+
+    handleForwardClick = () =>{
+        this.props.history.goForward();
+    }
+
+
+    handleBackClick = () =>{
+        // this.props.history.goBack();
+        this.props.history.go(-1);
+    }
+
+    handleRandomClick =() =>{
+        const random = Math.round(Math.random() * (10 -1) + 1)
+        this.props.history.push(`/videos?id=${random}`, {id: random});
+    }
+
+    render(){
+        return (
+            <div className="Page NotFound">
+                <h1>404</h1>
+                <h3 className="SadFace">:c</h3>
+                <h2>No hemos encontrado la pagina que buscabas</h2>
+                <button className="Button"
+                    onClick={this.handleForwardClick}
+                >
+                    Ir a la siguiente ruta --b 
+                </button>
+                <button className="Button"
+                    onClick={this.handleBackClick}                
+                >
+                    Ir a la ruta anterior a--
+                </button>
+                <button className="Button"
+                    onClick={this.handleRandomClick}                
+                >
+                    video random
+                </button>
+            </div>
+        )
+    }
+}
+
+export default withRouter(NotFound);
